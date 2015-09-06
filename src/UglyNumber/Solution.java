944c5aa515e42a6de9abd02b66025697056f1920
Kaushal <sbondada@buffalo.edu>
2015-09-06 19:23:51 -0400

    added the code to find if the number is ugly or not

---
+++ src/UglyNumber/Solution.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public boolean isUgly(int num) 
+    {
+       if (num!=0)
+        {
+            while(num % 2 ==0)     
+            {
+                num/=2;
+            }
+            while(num % 3 ==0)     
+            {
+                num/=3;
+            }
+            while(num % 5 ==0)     
+            {
+                num/=5;
+            }
+
+            if (num==1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }    
+    }
+}
