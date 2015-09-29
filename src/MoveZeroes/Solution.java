05481818463cdd0aff6cd91f02bcd0d5f36538dd
Kaushal <sbondada@buffalo.edu>
2015-09-29 00:03:12 -0400

    Moving zeroes to the last of the array

---
+++ src/MoveZeroes/Solution.java
@@ -0,0 +1,25 @@
+public class Solution 
+{
+    public void moveZeroes(int[] nums) 
+    {
+        int i=0;
+        int j=0;
+        while(j<nums.length)
+        {
+            if( nums[j]!=0)
+            {
+                int temp = nums[i]; 
+                nums[i] = nums[j];
+                nums[j] = temp; 
+                i++;
+            }
+            j++;
+        }
+        while(i<nums.length) 
+        {
+            nums[i] = 0;
+            i++;
+        }
+        return nums;
+    }
+}
